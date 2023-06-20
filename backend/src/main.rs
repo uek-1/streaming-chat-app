78713f035da54372b66d1768012b6421cb3665f0
Vivek <vivek@pop-os.localdomain>
2023-06-21 00:06:18 +0530

    Created websocket on back + front end.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::ws::{WebSocketUpgrade, WebSocket, Message},
+    routing::get,
+    response::{IntoResponse, Response},
+    Router,
+};
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, PartialEq, Serialize)]
+struct ChatMessage {
+    content: String,
 }
+
+#[tokio::main]
+async fn main() {
+    let app : Router<(), > = Router::new().route("/ws", get(handler));
+
+    let _ = axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(ws : WebSocketUpgrade) -> Response {
+    println!("handler called");
+    ws.on_upgrade(handle_socket)
+}
+
+//TODO: Recieve and Send messages!
+async fn handle_socket(mut socket : WebSocket) {
+    let test_msg = ChatMessage {
+        content: String::from("test"),
+    };
+    
+    for _i in 0..10 {
+        socket.send(Message::Text(
+            serde_json::to_string(&test_msg).unwrap()
+            ))
+            .await
+            .unwrap();
+    }
+
+    println!("Test Message Recieved!");
+}
+
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,48 @@
-<script>
-  export let test = ["test", "name"];
+<script language = "typescript">
+  import {onMount} from "svelte";
+  let ws;
+  
+  $: inputContent = "";
+  $: content = null;
+  $: receivedMessages = [];
+  
+  onMount (() => {
+    console.log(window.location.host.split(":"));
+    ws = new WebSocket(`ws://${window.location.host.split(':')[0]}:3000/ws`)
+    ws.addEventListener("message", handleMessage);
+    console.log("setup");
+    return () => {
+      ws.close();
+      console.log("closed");
+    }
+  });
+
+
+  function handleMessage(msg) {
+    console.log("test");
+    let data = JSON.parse(msg.data);
+    console.log(data);
+    content = data.content;
+    console.log(content);
+    receivedMessages.push(`${content}`);
+    receivedMessages = receivedMessages;
+  }
+
+  async function onClick(e) {
+    console.log("SENT", inputContent);
+    ws.send(inputContent);
+    inputContent = "";
+  }
+
 </script>
 
 <h1>CHAT:</h1>
 <table class = "chat-table"> 
-  {#each test as item}
+  {#each receivedMessages as item}
     <tr>{item}</tr>
   {/each}
 </table>
+<form on:submit={onClick}>
+  <input type="text" bind:value={inputContent}>
+  <button>"Chat"</button>
+</form>
