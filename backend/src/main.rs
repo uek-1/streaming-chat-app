913bd927f56f2e3738bd978dcf4a9b338f8648ac
Vivek <vivek@pop-os.localdomain>
2023-06-23 02:30:48 +0530

    Sent messages are received and sent back but only to the same socket.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
 use core::pin::Pin;
-use std::task::{Context, Poll};
-
+use std::{task::{Context, Poll}, collections::VecDeque};
+use tokio::sync::mpsc;
 
 use axum::{
     extract::ws::{WebSocketUpgrade, WebSocket, Message},
@@ -8,7 +8,7 @@ use axum::{
     response::{IntoResponse, Response},
     Router,
 };
-use futures_util::{sink::{Sink, SinkExt}, stream::{self, Stream, StreamExt, SplitSink, SplitStream}, Future};
+use futures_util::{sink::{Sink, SinkExt}, stream::{self, Stream, StreamExt, SplitSink, SplitStream}};
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, PartialEq, Serialize)]
@@ -16,15 +16,40 @@ struct ChatMessage {
     content: String,
 }
 
+impl From<String> for ChatMessage {
+    fn from(value: String) -> Self {
+        ChatMessage { content: value }
+    }
+}
+
 #[derive(Clone, PartialEq, Serialize)]
 struct ChatStream {
+    pub chat_messages : Box<VecDeque<String>>,
+}
+
+impl ChatStream {
+    pub fn new() -> Self {
+        ChatStream {
+            chat_messages : Box::new(VecDeque::new()),
+        }
+    }
+
+}
+
+impl Unpin for ChatStream {
+
 }
 
 impl Stream for ChatStream {
     type Item = String;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        Poll::Ready(None)
+        println!("{:?}", self.chat_messages.clone());
+        let chat_stream = self.get_mut();
+        match chat_stream.chat_messages.pop_front() {
+            Some(x) => Poll::Ready(Some(x)),
+            None => Poll::Pending,
+        }
     }
 }
 
@@ -44,6 +69,7 @@ impl Sink<String> for ChatStream {
     }
 
     fn start_send(self: Pin<&mut Self>, item: String) -> Result<(), Self::Error> {
+        self.get_mut().chat_messages.push_back(item);
         Ok(())
     }
 
@@ -66,15 +92,13 @@ async fn handler(ws : WebSocketUpgrade) -> Response {
 
 async fn handle_socket(mut socket: WebSocket) {
     let (mut socket_sender, mut socket_receiver) = socket.split();
-
-    let chat_stream = ChatStream{};
-    let (mut chat_stream_sender, mut chat_stream_receiver) = chat_stream.split();
-
-    tokio::spawn(write(socket_sender, chat_stream_receiver));
-    tokio::spawn(read(socket_receiver, chat_stream_sender));
+    
+    let (chat_message_sender, chat_message_receiver) = mpsc::channel::<String>(10); 
+    tokio::spawn(write(socket_sender,  chat_message_receiver));
+    tokio::spawn(read(socket_receiver, chat_message_sender));
 }
 
-async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: SplitSink<ChatStream, String>) {
+async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: mpsc::Sender<String>) {
     let mut socket_receiver = socket_receiver;
     let mut chat_message_sender = chat_message_sender;
     while let Some(chat_msg) = socket_receiver.next().await {
@@ -93,10 +117,10 @@ async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: Spli
         } 
     }
     
-    println!("END WHILE");
+    println!("END READ");
 }
 
-async fn write(socket_sender: SplitSink<WebSocket, Message>, chat_message_receiver: SplitStream<ChatStream>) {
+async fn write(socket_sender: SplitSink<WebSocket, Message>, chat_message_receiver: mpsc::Receiver<String>) {
     let mut socket_sender = socket_sender;
     let mut chat_message_receiver = chat_message_receiver;
 
@@ -111,13 +135,16 @@ async fn write(socket_sender: SplitSink<WebSocket, Message>, chat_message_receiv
 
     println!("FIRST MESSAGE RECIEVED");
     
-    while let Some(chat_msg) = chat_message_receiver.next().await {
+    while let Some(chat_msg) = chat_message_receiver.recv().await {
+
         socket_sender.send(Message::Text(
-            serde_json::to_string(&chat_msg).unwrap()
+            serde_json::to_string(&ChatMessage::from(chat_msg)).unwrap()
         ))
         .await
         .unwrap();
     }
+
+    println!("END WRITE");
 
 }
 
