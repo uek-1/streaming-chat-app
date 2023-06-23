928594132fd126d0a056b02d99332aad807c83cf
Vivek <vivek@pop-os.localdomain>
2023-06-23 12:00:42 +0530

    Messages sent across multiple clients successfully.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,9 @@
 use core::pin::Pin;
-use std::{task::{Context, Poll}, collections::VecDeque};
-use tokio::sync::mpsc;
+use std::{task::{Context, Poll}, collections::VecDeque, sync::{Arc, Mutex}};
+use tokio::sync::broadcast;
 
 use axum::{
-    extract::ws::{WebSocketUpgrade, WebSocket, Message},
+    extract::{ws::{WebSocketUpgrade, WebSocket, Message}, State},
     routing::get,
     response::{IntoResponse, Response},
     Router,
@@ -22,62 +22,26 @@ impl From<String> for ChatMessage {
     }
 }
 
-#[derive(Clone, PartialEq, Serialize)]
-struct ChatStream {
-    pub chat_messages : Box<VecDeque<String>>,
+#[derive(Clone)]
+struct ChatStreamState {
+    sender : broadcast::Sender<String>,
+    receiver : Arc<Mutex<broadcast::Receiver<String>>>
 }
 
-impl ChatStream {
-    pub fn new() -> Self {
-        ChatStream {
-            chat_messages : Box::new(VecDeque::new()),
-        }
-    }
 
-}
-
-impl Unpin for ChatStream {
-
-}
-
-impl Stream for ChatStream {
-    type Item = String;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        println!("{:?}", self.chat_messages.clone());
-        let chat_stream = self.get_mut();
-        match chat_stream.chat_messages.pop_front() {
-            Some(x) => Poll::Ready(Some(x)),
-            None => Poll::Pending,
-        }
-    }
-}
-
-impl Sink<String> for ChatStream {
-    type Error = &'static str;
-
-    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-         Poll::Ready(Ok(()))   
-    }
-
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn start_send(self: Pin<&mut Self>, item: String) -> Result<(), Self::Error> {
-        self.get_mut().chat_messages.push_back(item);
-        Ok(())
-    }
-
-}
 
 #[tokio::main]
 async fn main() {
-    let app : Router<(), > = Router::new().route("/ws", get(handler));
+    let (chat_message_sender, chat_message_receiver) = broadcast::channel::<String>(10); 
+    
+    let state = ChatStreamState {
+        sender : chat_message_sender,
+        receiver : Arc::new(Mutex::new(chat_message_receiver))
+    };
+
+    let app : Router<(), > = Router::new()
+        .route("/ws", get(handler))
+        .with_state(state);
 
     let _ = axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -85,22 +49,23 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler(ws : WebSocketUpgrade) -> Response {
+async fn handler(ws : WebSocketUpgrade, State(state) : State<ChatStreamState>) -> Response {
     println!("handler called");
-    ws.on_upgrade(handle_socket)
+    let chat_sender = state.sender;
+    let chat_receiver = chat_sender.subscribe();
+    ws.on_upgrade(|socket| handle_socket(socket, chat_sender, chat_receiver))
 }
 
-async fn handle_socket(mut socket: WebSocket) {
+async fn handle_socket(mut socket: WebSocket, chat_sender: broadcast::Sender<String>, chat_receiver: broadcast::Receiver<String>) {
     let (mut socket_sender, mut socket_receiver) = socket.split();
-    
-    let (chat_message_sender, chat_message_receiver) = mpsc::channel::<String>(10); 
-    tokio::spawn(write(socket_sender,  chat_message_receiver));
-    tokio::spawn(read(socket_receiver, chat_message_sender));
+ 
+    tokio::spawn(write(socket_sender, chat_receiver));
+    tokio::spawn(read(socket_receiver, chat_sender));
 }
 
-async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: mpsc::Sender<String>) {
+async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: broadcast::Sender<String>) {
     let mut socket_receiver = socket_receiver;
-    let mut chat_message_sender = chat_message_sender;
+    let chat_message_sender = chat_message_sender;
     while let Some(chat_msg) = socket_receiver.next().await {
         let msg = if let Ok(msg) = chat_msg {
             println!("{:?}", msg);
@@ -112,7 +77,6 @@ async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: mpsc
 
         if let Message::Text(msg_text) = msg {
             chat_message_sender.send(msg_text)
-                .await
                 .unwrap();
         } 
     }
@@ -120,7 +84,7 @@ async fn read(socket_receiver: SplitStream<WebSocket>, chat_message_sender: mpsc
     println!("END READ");
 }
 
-async fn write(socket_sender: SplitSink<WebSocket, Message>, chat_message_receiver: mpsc::Receiver<String>) {
+async fn write(socket_sender: SplitSink<WebSocket, Message>, chat_message_receiver: broadcast::Receiver<String>) {
     let mut socket_sender = socket_sender;
     let mut chat_message_receiver = chat_message_receiver;
 
@@ -135,7 +99,7 @@ async fn write(socket_sender: SplitSink<WebSocket, Message>, chat_message_receiv
 
     println!("FIRST MESSAGE RECIEVED");
     
-    while let Some(chat_msg) = chat_message_receiver.recv().await {
+    while let Ok(chat_msg) = chat_message_receiver.recv().await {
 
         socket_sender.send(Message::Text(
             serde_json::to_string(&ChatMessage::from(chat_msg)).unwrap()
